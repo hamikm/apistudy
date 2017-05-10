4a865391adc21ccb7e9cb3924d7aa2f92e216d14
root <root@spacex-dev>
2017-05-10 03:24:05 +0000

    POST endpoing works

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
+import json
+from datetime import datetime
 
 class Listing(db.Model):
-    '''Table of listings for SpaceBnB'''
+    '''Define table of listings for SpaceBnB'''
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(20))
     title = db.Column(db.String(140))
@@ -10,8 +12,37 @@ class Listing(db.Model):
     locationX = db.Column(db.Float)
     locationY = db.Column(db.Float)
 
+    DATEFORMAT = "%Y-%m-%dT%H:%M:%S"
+    
     def __repr__(self):
         return ('Listing: {} posted {} at ({}, {}). It expires {}'.format(
             self.user, self.title, self.locationX, self.locationY, self.expiration))
 
-    
+    @classmethod
+    def parseDate(cls, dateStr):
+        '''Convert string date to datetime object'''
+        try:
+            return dateStr and datetime.strptime(dateStr, cls.DATEFORMAT)
+        except:
+            return None
+            
+    @classmethod
+    def validate(cls, listingJson):
+        '''Validates json listings'''
+        user = listingJson.get('user')
+        title = listingJson.get('title')
+        description = listingJson.get('description')
+        expirationStr = listingJson.get('expiration')
+        locationX = listingJson.get('location') and listingJson.get('location').get('x')
+        locationY = listingJson.get('location') and listingJson.get('location').get('y')
+        expiration = cls.parseDate(expirationStr)
+        
+        return (user and type(user) == unicode
+            and title and type(title) == unicode
+            and description and type(description) == unicode
+            and expiration and type(expiration) == datetime
+            and locationX and type(locationX) == float
+            and locationY and type(locationY) == float
+        )
+
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
-from app import app
+from datetime import datetime
+
+from app import app, models, db
+from flask import request
 
 @app.route('/health')
 def health():
-    return "ok"
+    return 'ok'
+
+@app.route('/api/listings', methods=['POST'])
+def postListing():
+    content = request.get_json(silent=True)
+    
+    if models.Listing.validate(content):
+        listing = models.Listing(
+            user=content.get('user'),
+            title=content.get('title'),
+            description=content.get('description'),
+            expiration=models.Listing.parseDate(content.get('expiration')),
+            locationX=content.get('location').get('x'),
+            locationY=content.get('location').get('y')
+        )
+
+        try:
+            db.session.add(listing)
+            db.session.commit()
+        except Exception as e:
+            print '============> ', e 
+            return 'nay' # TODO return 404
+
+        return 'yayyyy'
+    else:
+        return 'nay'
 
