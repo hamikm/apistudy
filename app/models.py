8dcf6d66850dd6274987f64cab7c843abb3af255
root <root@spacex-dev>
2017-05-10 01:42:54 +0000

    db working

--- app/models.py
+++ app/models.py
@@ -1,13 +1,17 @@
 from app import db
 
-
-class TUser(db.Model):
-    '''Test database table creation'''
+class Listing(db.Model):
+    '''Table of listings for SpaceBnB'''
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    user = db.Column(db.String(20))
+    title = db.Column(db.String(140))
+    description = db.Column(db.String(1024))
+    expiration = db.Column(db.DateTime)
+    locationX = db.Column(db.Float)
+    locationY = db.Column(db.Float)
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return ('Listing: {} posted {} at ({}, {}). It expires {}'.format(
+            self.user, self.title, self.locationX, self.locationY, self.expiration))
 
     
